3941edec150b01008e66990a2c2c56adf7ac7556
alec <alec@graves.tech>
2019-03-01 19:10:04 -0500

    add basic tello control code

--- real_time.py
+++ real_time.py
@@ -8,6 +8,11 @@ import wave
 import subprocess
 import shlex
 import pyaudio
+import os
+from queue import Queue
+import tello
+import time
+
 
 import numpy as np
 try:
@@ -15,17 +20,34 @@ try:
 except ImportError:
     from pipes import quote
 
-LM_WEIGHT = 1.5
-VALID_WORD_COUNT_WEIGHT = 2.25
-N_FEATURES = 26
-N_CONTEXT = 9
-BEAM_WIDTH = 512
+# Beam width used in the CTC decoder when building candidate transcriptions
+BEAM_WIDTH = 500
 
-model = '/home/nihadern/voice_recog/models/output_graph.rounded.pbmm'
-micro = '/home/nihadern/voice_recog/male.wav'
-alpha = '/home/nihadern/voice_recog/models/alphabet.txt'
-trie = '/home/nihadern/voice_recog/models/trie'
-lm = '/home/nihadern/voice_recog/models/lm.binary'
+# The alpha hyperparameter of the CTC decoder. Language Model weight
+LM_WEIGHT = 1.50
+
+# Valid word insertion weight. This is used to lessen the word insertion penalty
+# when the inserted word is part of the vocabulary
+VALID_WORD_COUNT_WEIGHT = 2.10
+
+
+# These constants are tied to the shape of the graph used (changing them changes
+# the geometry of the first layer), so make sure you use the same constants that
+# were used during training
+
+# Number of MFCC features to use
+N_FEATURES = 26
+
+# Size of the context window used for producing timesteps in the input vector
+N_CONTEXT = 9
+
+
+MODELS_PATH = '../models'
+
+model = os.path.join(MODELS_PATH, 'output_graph.rounded.pbmm')
+alpha = os.path.join(MODELS_PATH, 'alphabet.txt')
+trie = os.path.join(MODELS_PATH, 'trie')
+lm = os.path.join(MODELS_PATH, 'lm.binary')
 
 
 
@@ -42,27 +64,101 @@ import pyaudio
 
 fs= 16000
 
-CHUNKSIZE =  16000*10 # fixed chunk size
+CHUNK = 4096
+
+RECORD_SECONDS = 5
+#16000*5 # fixed chunk size
+
+
 
 # initialize portaudio
+
+
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=fs, input=True, frames_per_buffer=CHUNKSIZE)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=fs, input=True, frames_per_buffer=CHUNK)
 
-# do this as long as you want fresh samples
-data = stream.read(CHUNKSIZE)
-numpydata = np.fromstring(data, dtype=np.int16)
+start_time = time.time()
 
-print(len(data))
-print(len(numpydata))
+connection = tello.Tello()
+connection.send("command")
 
-# close stream
-stream.stop_stream()
-stream.close()
-p.terminate()
+print(connection.send("battery?"))
 
-print("Done")
+try:
+	while True:
+		# do this as long as you want fresh samples
+		numpydata = []
+		text = input("Press [ENTER] to continue...")
+		print(connection.send("battery?"))
+		stream.start_stream()
+		for i in range(0, int(fs / CHUNK * RECORD_SECONDS)):
+			data = stream.read(CHUNK)
+			numpydata.append(np.fromstring(data, dtype=np.int16))
+		stream.stop_stream()
+
+		numpydata = np.concatenate(numpydata)
+		print(numpydata.shape)
+
+		# ~ if time.time() - start_time > 30:
+			# ~ connection.send("land")
+
+		spoken = str(ds.stt(numpydata, fs))
+		print(spoken)
+		
+		def contains(s, x):
+			for word in x:
+				if word in s:
+					return True
+			return False
+		
+		if "land" in spoken:
+			print("landing command recieved...")
+			connection.send("land")
+		elif contains(spoken, ["emergency", "emergenci", "he mergency", "abort", "a board", "aboart", "abandon ship", "kill now", "stop now"]):
+			connection.send("emergency")
+		elif contains(spoken, ["regina take off", "regina take ouff", "regina take opf"]):
+			print("about to send take off command...")
+			start_time = time.time()
+			connection.send("takeoff")
+		elif "regina twist" in spoken:
+			print("about to send flip command...")
+			connection.send("flip b")
+		
+		
+		
+except KeyboardInterrupt:
+	# close stream
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+	
 
 
-print(ds.stt(numpydata, fs))
-print('Done')
-#/usr/local/lib/python3.5/dist-packages/deepspeech
+# ~ print(len(data))
+# ~ print(len(numpydata))
+
+# ~ # close stream
+# ~ stream.stop_stream()
+# ~ stream.close()
+# ~ p.terminate()
+
+# ~ print("Done")
+
+
+
+# ~ print('Done')
+# ~ #/usr/local/lib/python3.5/dist-packages/deepspeech
+# ~ print(len(data))
+# ~ print(len(numpydata))
+
+# ~ # close stream
+# ~ stream.stop_stream()
+# ~ stream.close()
+# ~ p.terminate()
+
+# ~ print("Done")
+
+
+
+# ~ print('Done')
+# ~ #/usr/local/lib/python3.5/dist-packages/deepspeech
